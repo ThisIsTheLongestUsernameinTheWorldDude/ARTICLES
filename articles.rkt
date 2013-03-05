56a99987bb9c488c3aa0291985dde732269a72b1
BATMAN <cpwest11@gmail.com>
2013-03-05 13:00:40 -0600

    Y

--- articles.rkt
+++ articles.rkt
@@ -2,20 +2,24 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname articles) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 (require 2htdp/batch-io)
-(define file-words (read-words "ttt.txt"))
-(define-struct file (list counter))
-(define (remove-articles l)
-  (remove-articles-internal (make-file l 0)))
+(define file-words (read-words/line "ttt.txt"))
+(check-expect (remove-articles file-words) (list
+ (list "Why" "does" "girl" "sail?")
+ (list "Ships" "endure!")
+ (list "Desolation," "life," "and" "courage.")
+ (list "Stormy," "dead" "winds" "calmly" "pull" "old," "rough" "mast.")))
 (define (article? l)
 (cond
   [(or (string=? l"a") (or (string=? l"an") (or (string=? l"the") (or (string=? l"those") (string=? l"these"))))) true]
   [(false? (or (string=? l"a") (or (string=? l"an") (or (string=? l"the") (or (string=? l"those") (string=? l"these")))))) false]
   ))
-(define (remove-articles-internal l)
+(define (remove-articles l)
   (cond
-  [(file? l)(cond 
-    [(empty?(first (file-list l))) empty]
-    [(article? (first(file-list l)))  (remove-articles-internal (make-file (rest(file-list l)) (+ (file-counter l) 1)))]
-    [else (make-file (cons (first(file-list l)) (remove-articles-internal (rest(file-list l)))) (file-counter l))])]
-  [(cons? l) "wtf"]
-  [else "wat"]))          
+    [(empty? l) empty]
+    [else (cons (remove-articles-internal (first l)) (remove-articles (rest l)))]))
+    
+(define (remove-articles-internal l)
+  (cond 
+    [(empty? l) empty]
+    [(article? (first l))  (remove-articles-internal (rest l))]
+    [else (cons (first l) (remove-articles-internal (rest l)))]))
